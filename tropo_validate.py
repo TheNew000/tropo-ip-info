cd07b7bd4f6198c8fc09c4216bca112aef1c56d9
TheNew000 <mahkatorocks@gmail.com>
2016-11-17 21:25:10 -0500

    added further validations

--- tropo_validate.py
+++ tropo_validate.py
@@ -11,6 +11,8 @@ process = subprocess.Popen(["nslookup", "-q=TXT", "_netblocks.tropo.com", "8.8.8
 # Here I split the information wherever there is a new line allowing me to access '_netblocks.tropo.com\ttext = "159.122.242.16/28"' as a single item in a list
 data = process.communicate()[0].split('\n')
 
+valid = False
+
 # Validation checks if the pattern discovered by the re.findall() function is a valid IP Address and not just a random coincidence.  We check that we have 5 groups of numbers as well as whether those numbers are within a valid range for an accurate IP Address.  It returns a boolean used in the if statement on line 28
 def validate_ip(num):
     test = re.findall("\d{1,3}", str(num))
@@ -24,10 +26,19 @@ def validate_ip(num):
 # I create a list and then use a regex to search for IP addresses that match the required format.  When one is found and it passes validation it is added to the ip_arr along with the tropo string
 ip_arr = []
 for info in data:
-    ip_add = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}", info)
-    if ip_add and validate_ip(ip_add):
-        ip_arr.append('tropo,' + ip_add[0])
-print ip_arr
+    if "** server can't find" not in info:
+        ip_add = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2}", info)
+        if ip_add and validate_ip(ip_add):
+            ip_arr.append('tropo,' + ip_add[0])
+            valid = True
+    else:
+        print "Invalid address server cannot connect"
+        valid = False
+if len(ip_arr) > 0:
+    if valid:
+        print ip_arr
+else:
+    print "No valid IP Addresses were found"
 
 if __name__ == '__main__':
     app.run(debug=True)
